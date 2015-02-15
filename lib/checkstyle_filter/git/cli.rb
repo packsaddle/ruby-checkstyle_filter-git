5e609476dcbf0dc29aa816728c09d388a03b56c3
sanemat <o.gata.ken@gmail.com>
2015-02-15 17:32:27 +0900

    feat(diff): fake implementation

--- lib/checkstyle_filter/git/cli.rb
+++ lib/checkstyle_filter/git/cli.rb
@@ -5,6 +5,25 @@ module CheckstyleFilter
     class CLI < Thor
       require 'checkstyle_filter/git'
 
+      def self.exit_on_failure?
+        true
+      end
+
+      desc 'diff', 'Filter using `git diff`'
+      option :data
+      def diff(_commit_ish = 'origin/master')
+        data = \
+          if options[:data]
+            options[:data]
+          elsif !$stdin.tty?
+            ARGV.clear
+            ARGF.read
+          end
+
+        abort if !data || data.empty?
+        puts data
+      end
+
       desc 'version', 'Show the CheckstyleFilter/Git version'
       map %w(-v --version) => :version
 
