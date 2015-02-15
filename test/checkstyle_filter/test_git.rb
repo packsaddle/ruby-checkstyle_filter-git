23b7200b1ab25931104de9e36be9a966040eb2e6
sanemat <o.gata.ken@gmail.com>
2015-02-15 12:13:33 +0900

    style(rubocop): use module style

--- test/checkstyle_filter/test_git.rb
+++ test/checkstyle_filter/test_git.rb
@@ -1,11 +1,13 @@
 require 'minitest_helper'
 
-class CheckstyleFilter::TestGit < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::CheckstyleFilter::Git::VERSION
-  end
+module CheckstyleFilter
+  class TestGit < Minitest::Test
+    def test_that_it_has_a_version_number
+      refute_nil ::CheckstyleFilter::Git::VERSION
+    end
 
-  def test_it_does_something_useful
-    assert false
+    def test_it_does_something_useful
+      assert false
+    end
   end
 end
