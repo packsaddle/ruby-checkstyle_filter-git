8d8beee4a54ade0bec7c495e144e2b6d089ff408
sanemat <o.gata.ken@gmail.com>
2015-02-21 22:06:10 +0900

    refactor(filter): move logic to filter class

--- lib/checkstyle_filter/git/cli.rb
+++ lib/checkstyle_filter/git/cli.rb
@@ -26,23 +26,7 @@ module CheckstyleFilter
 
         command = ['git', 'diff', '--no-color', commit_ish].compact
         git_diff, _, _ = Open3.capture3(*command)
-        parsed = ::Git::Diff::Parser.parse(git_diff)
-
-        # TODO: split to class
-        require 'rexml/document'
-        document = REXML::Document.new data
-        document.elements.each('/checkstyle/file') do |file_element|
-          file_name = file_element.attribute('name').value
-          next unless file_element_file_in_git_diff?(file_name, parsed)
-          file_element.elements.each('error') do |error_element|
-            line = error_element.attribute('line') && error_element.attribute('line').value.to_i
-            if file_element_error_line_no_in_modified?(file_name, parsed, line)
-              error_element.remove
-            end
-          end
-        end
-
-        puts document.to_s
+        puts ::CheckstyleFilter::Git::Filter.filter(data, git_diff)
       end
 
       desc 'version', 'Show the CheckstyleFilter/Git version'
@@ -50,35 +34,6 @@ module CheckstyleFilter
 
       def version
         puts "CheckstyleFilter/Git version #{::CheckstyleFilter::Git::VERSION}"
-      end
-
-      no_commands do
-        def file_element_file_in_git_diff?(file_name, patches)
-          require 'pathname'
-          diff_files = patches.map(&:file)
-          diff_files
-            .map { |file| Pathname.new(file).expand_path }
-            .include?(Pathname.new(file_name).expand_path)
-        end
-
-        def file_element_error_line_no_in_modified?(file_name, patches, line_no)
-          require 'pathname'
-          diff_patches = patches
-                         .select do |patch|
-            Pathname.new(patch.file).expand_path \
-              == Pathname.new(file_name).expand_path
-          end
-          return false if diff_patches.empty?
-
-          modified_lines = Set.new
-          diff_patches.map do |patch|
-            patch.changed_lines.map do |line|
-              modified_lines << line.number
-            end
-          end
-
-          modified_lines.include?(line_no)
-        end
       end
     end
   end
---
+++ lib/checkstyle_filter/git/filter.rb
@@ -0,0 +1,51 @@
+module CheckstyleFilter
+  module Git
+    class Filter
+      def self.filter(data, git_diff)
+        parsed = ::Git::Diff::Parser.parse(git_diff)
+
+        require 'rexml/document'
+        document = REXML::Document.new data
+        document.elements.each('/checkstyle/file') do |file_element|
+          file_name = file_element.attribute('name').value
+          next unless file_element_file_in_git_diff?(file_name, parsed)
+          file_element.elements.each('error') do |error_element|
+            line = error_element.attribute('line') && error_element.attribute('line').value.to_i
+            if file_element_error_line_no_in_modified?(file_name, parsed, line)
+              error_element.remove
+            end
+          end
+        end
+
+        document.to_s
+      end
+
+      def self.file_element_file_in_git_diff?(file_name, patches)
+        require 'pathname'
+        diff_files = patches.map(&:file)
+        diff_files
+          .map { |file| Pathname.new(file).expand_path }
+          .include?(Pathname.new(file_name).expand_path)
+      end
+
+      def self.file_element_error_line_no_in_modified?(file_name, patches, line_no)
+        require 'pathname'
+        diff_patches = patches
+                         .select do |patch|
+          Pathname.new(patch.file).expand_path \
+              == Pathname.new(file_name).expand_path
+        end
+        return false if diff_patches.empty?
+
+        modified_lines = Set.new
+        diff_patches.map do |patch|
+          patch.changed_lines.map do |line|
+            modified_lines << line.number
+          end
+        end
+
+        modified_lines.include?(line_no)
+      end
+    end
+  end
+end
