07d6471dea53c64db1e214e39906fec8139117f5
sanemat <o.gata.ken@gmail.com>
2015-02-22 00:43:06 +0900

    test(filter): .same_file?

--- test/test_filter.rb
+++ test/test_filter.rb
@@ -1,9 +1,16 @@
 require_relative 'helper'
 
 class FilterTest < Test::Unit::TestCase
-  test '' do
-    assert do
-      true == false
+  sub_test_case '.same_file?' do
+    test 'same' do
+      assert do
+        CheckstyleFilter::Git::Filter.same_file?('same', 'equal/../same')
+      end
+    end
+    test 'not same' do
+      assert do
+        !CheckstyleFilter::Git::Filter.same_file?('same', 'not/same')
+      end
     end
   end
 end
