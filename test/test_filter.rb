8be59590b3f9648d670dfba3c364e662b100eb0d
sanemat <o.gata.ken@gmail.com>
2015-02-22 01:07:50 +0900

    test(filter) .file_element_file_in_git_diff?

--- test/test_filter.rb
+++ test/test_filter.rb
@@ -1,15 +1,38 @@
 require_relative 'helper'
 
-class FilterTest < Test::Unit::TestCase
-  sub_test_case '.same_file?' do
-    test 'same' do
-      assert do
-        CheckstyleFilter::Git::Filter.same_file?('same', 'equal/../same')
+module CheckstyleFilter
+  module Git
+    class FilterTest < Test::Unit::TestCase
+      sub_test_case '.same_file?' do
+        test 'same' do
+          assert do
+            Filter.same_file?('same', 'equal/../same')
+          end
+        end
+        test 'not same' do
+          assert do
+            !Filter.same_file?('same', 'not/same')
+          end
+        end
       end
-    end
-    test 'not same' do
-      assert do
-        !CheckstyleFilter::Git::Filter.same_file?('same', 'not/same')
+
+      sub_test_case '.file_element_file_in_git_diff?' do
+        patches = ::Git::Diff::Parser::Patches[
+          ::Git::Diff::Parser::Patch.new('', file: 'path/to/this_one'),
+          ::Git::Diff::Parser::Patch.new('', file: 'dammy_one')
+        ]
+        test 'included' do
+          file = 'path/to/this_one'
+          assert do
+            Filter.file_element_file_in_git_diff?(file, patches)
+          end
+        end
+        test 'not included' do
+          file = 'not_included'
+          assert do
+            !Filter.file_element_file_in_git_diff?(file, patches)
+          end
+        end
       end
     end
   end
