319de433145f766f1f1d468dcbec3c021ae6d3c6
sanemat <o.gata.ken@gmail.com>
2015-02-23 23:17:35 +0900

    refactor(filter): use new apis

--- lib/checkstyle_filter/git/filter.rb
+++ lib/checkstyle_filter/git/filter.rb
@@ -10,10 +10,12 @@ module CheckstyleFilter
         document = REXML::Document.new data
         document.elements.each('/checkstyle/file') do |file_element|
           file_name = file_element.attribute('name') && file_element.attribute('name').value
-          if file_in_patches?(file_name, patches)
+          file = file_relative_path(file_name)
+          patch = patches.find_patch_by_file(file)
+          if patch
             file_element.elements.each('error') do |error_element|
               line = error_element.attribute('line') && error_element.attribute('line').value.to_i
-              unless file_element_error_line_no_in_patches?(file_name, patches, line)
+              unless patch.changed_line_numbers.include?(line)
                 error_element.remove
               end
             end
@@ -27,30 +29,12 @@ module CheckstyleFilter
         document.to_s
       end
 
-      def self.file_in_patches?(file_name, patches)
-        patches
-          .map(&:file)
-          .map { |file| Pathname.new(file).expand_path }
-          .include?(Pathname.new(file_name).expand_path)
-      end
-
-      def self.file_element_error_line_no_in_patches?(file_name, patches, line_no)
-        diff_patches = patches
-                       .select { |patch| same_file?(patch.file, file_name) }
-        return false if diff_patches.empty?
-
-        modified_lines = Set.new
-        diff_patches.map do |patch|
-          patch.changed_lines.map do |line|
-            modified_lines << line.number
-          end
+      def self.file_relative_path(file_name)
+        if Pathname.new(file_name).absolute?
+          Pathname.new(file_name).relative_path_from(Pathname.new(Dir.pwd)).to_s
+        else
+          Pathname.new(file_name).relative_path_from(Pathname.new('.')).to_s
         end
-
-        modified_lines.include?(line_no)
-      end
-
-      def self.same_file?(one, other)
-        Pathname.new(one).expand_path == Pathname.new(other).expand_path
       end
     end
   end
--- test/test_filter.rb
+++ test/test_filter.rb
@@ -3,57 +3,6 @@ require_relative 'helper'
 module CheckstyleFilter
   module Git
     class FilterTest < Test::Unit::TestCase
-      sub_test_case '.same_file?' do
-        test 'same' do
-          assert do
-            Filter.same_file?('same', 'equal/../same')
-          end
-        end
-        test 'not same' do
-          assert do
-            !Filter.same_file?('same', 'not/same')
-          end
-        end
-      end
-
-      sub_test_case '.file_element_file_in_git_diff?' do
-        patches = ::GitDiffParser::Patches[
-          ::GitDiffParser::Patch.new('', file: 'path/to/this_one'),
-          ::GitDiffParser::Patch.new('', file: 'dammy_one')
-        ]
-        test 'included' do
-          file = 'path/to/this_one'
-          assert do
-            Filter.file_in_patches?(file, patches)
-          end
-        end
-        test 'not included' do
-          file = 'not_included'
-          assert do
-            !Filter.file_in_patches?(file, patches)
-          end
-        end
-      end
-
-      sub_test_case '.file_element_error_line_no_in_patches?' do
-        diff = File.read('./test/support/fixtures/git_diff_test.txt')
-        patches = ::GitDiffParser.parse(diff)
-        test 'included' do
-          file = 'example/invalid.rb'
-          line_no = 3 # error and diff included
-          assert do
-            Filter.file_element_error_line_no_in_patches?(file, patches, line_no)
-          end
-        end
-        test 'not included' do
-          file = 'lib/checkstyle_filter/git/cli.rb'
-          line_no = 65 # error but not diff included
-          assert do
-            !Filter.file_element_error_line_no_in_patches?(file, patches, line_no)
-          end
-        end
-      end
-
       sub_test_case '.filter' do
         diff = File.read('./test/support/fixtures/git_diff_test.txt')
         before_filter = File.read('./test/support/fixtures/checkstyle_before_filter.xml')
